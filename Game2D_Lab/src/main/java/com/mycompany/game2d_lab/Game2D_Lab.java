75ca46129e0d8549c858e0a890944f5f8c4575ed
polawich <42580909+polawich@users.noreply.github.com>
2022-12-13 22:25:14 +0700

    update file

---
+++ Game2D_Lab/src/main/java/com/mycompany/game2d_lab/Game2D_Lab.java
@@ -0,0 +1,17 @@
+/*
+ * Click nbfs://nbhost/SystemFileSystem/Templates/Licenses/license-default.txt to change this license
+ * Click nbfs://nbhost/SystemFileSystem/Templates/Project/Maven2/JavaApp/src/main/java/${packagePath}/${mainClassName}.java to edit this template
+ */
+
+package com.mycompany.game2d_lab;
+
+/**
+ *
+ * @author polawich
+ */
+public class Game2D_Lab {
+
+    public static void main(String[] args) {
+        System.out.println("Hello World!");
+    }
+}
